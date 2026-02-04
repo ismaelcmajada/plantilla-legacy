818f2cb7e4508b8310968b37643894675e1edd95
ismaelcmajada <ismaelcmajada@gmail.com>
2026-02-04 16:41:48 +0000

    update home route constant from usuarios to user

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard/usuarios';
+    public const HOME = '/dashboard/user';
 
     /**
      * Define your route model bindings, pattern filters, etc.
